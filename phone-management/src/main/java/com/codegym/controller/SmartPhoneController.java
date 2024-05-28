a4594944167752d3b7e7141f82eec90620ff0c6f
thien306 <nguyenvantho306@gmail.com>
2024-05-28 22:38:23 +0700

    init commit

---
+++ phone-management/src/main/java/com/codegym/controller/SmartPhoneController.java
@@ -0,0 +1,46 @@
+package com.codegym.controller;
+
+import com.codegym.model.Smartphone;
+import com.codegym.service.ISmartphoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping("/api/smartphones")
+public class SmartPhoneController {
+
+    @Autowired
+    private ISmartphoneService smartphoneService;
+
+
+    @GetMapping
+    public ResponseEntity<Iterable<Smartphone>> getAllSmartphones() {
+        Iterable<Smartphone> smartphones = smartphoneService.findAll();
+        if (!smartphones.iterator().hasNext()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(smartphones, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Smartphone> createSmartphone(@RequestBody Smartphone smartphone) {
+        return new ResponseEntity<>(smartphoneService.save(smartphone), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Smartphone> deleteSmartphone(@PathVariable Long id) {
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        smartphoneService.remove(id);
+        return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
+    }
+}
